4c0083e11916fb485b985189ded255d673614178
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-20 19:15:26 +0400

    Finish project

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int term = 0;
+    int term2 = 1;
+    int nextTerm;
+    int numberOfTerms;
+    cout << "Enter how many terms in addition to the first 3 terms do you want to see from the Fibonacci sequence: ";
+    cin >> numberOfTerms;
+    cout << "Fibonacci sequence: " << term << ", " << term2;
+    for (int i = numberOfTerms; i > 0; i--)
+    {
+        nextTerm = term + term2;
+        cout << ", " << nextTerm;
+        term = term2;
+        term2 = nextTerm;
+    }
+
     return 0;
 }
